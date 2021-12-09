c06b9ee3110acbd84c07a10ea3bdbdd76fddc357
Mayra <mayra@iMac-de-Mayra.local>
2021-12-09 13:32:15 -0800

    Updates

---
+++ precache-manifest.c485fc64ae4c9f4053b669fd506e658d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae0d6bd40d44fdcfa370d3d850a8ed0",
+    "url": "/bank/index.html"
+  },
+  {
+    "revision": "9a8c9d6d0d9238e938ea",
+    "url": "/bank/static/css/2.b3d21b21.chunk.css"
+  },
+  {
+    "revision": "ef52de7a5c4c146eac37",
+    "url": "/bank/static/css/main.862d2e67.chunk.css"
+  },
+  {
+    "revision": "9a8c9d6d0d9238e938ea",
+    "url": "/bank/static/js/2.67546ff8.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/bank/static/js/2.67546ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef52de7a5c4c146eac37",
+    "url": "/bank/static/js/main.76fc6a97.chunk.js"
+  },
+  {
+    "revision": "ac6e12e594cdfa704564",
+    "url": "/bank/static/js/runtime-main.39ec1bdb.js"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/bank/static/media/dbank.adca41d3.png"
+  }
+]);
